e5c98931839f2317c2db4e97129fd581b8367a42
Gamerk786po <abdullahpper@gmail.com>
2025-07-09 02:23:18 +0500

    The loading sequence added

--- Frontend/src/components/body/body.tsx
+++ Frontend/src/components/body/body.tsx
@@ -1,45 +1,82 @@
 import { useEffect, useState } from "react";
 import Card from "./card";
+import { motion } from "framer-motion"; // for using framer motion 
+import { Typewriter } from "react-simple-typewriter"; // for using typing animation
+
 // Interface for the 20 got pokemons
 interface fetchedPokemonsInterface {
   name: string;
   url: string;
 }
 
-// The component for body https://pokeapi.co/api/v2/pokemon/?limit=151&offset=0
+// The component for body
 const Body = () => {
-  // State management
+  // State managements:
 
-  // For pokemons data
+  // Pokemons data
   const [pokemons, setPokemons] = useState<fetchedPokemonsInterface[]>([]);
 
-  // For offset of api which will be altered by 20
+  // Offset of api which will be altered by 20
   const [offset, setOffSet] = useState<number>(0);
 
-  // The function for api calls for 20 pokemons in pagination.
+  // isLoading
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  // The function for api call of 20 pokemons in pagination.
   const getPokemons = async () => {
     const res = await fetch(
       `https://pokeapi.co/api/v2/pokemon?limit=20&${offset}`
     );
     const json = await res.json();
     setPokemons(json.results);
+    setTimeout(() => setIsLoading(false), 1700);
   };
 
-  // The useEffect for api requests
+  // The useEffect for API requests
   useEffect(() => {
     getPokemons();
   }, []);
 
   return (
     <>
-      {/* The container for containing the pokemon cards */}
-      <div className="flex justify-center items-center flex-wrap gap-15 mt-10">
-        {pokemons.map((pokemon) => {
-          const id = pokemon.url.split("/")[6];
-          const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
-          return <Card name={`${pokemon.name}`} imgUrl={`${url}`} />;
-        })}
-      </div>
+      {isLoading ? (
+        // The container for loading screen
+        <div className="flex justify-center items-center flex-col overflow-auto">
+          <img
+            className="h-40"
+            src="/loading-gif/Pikachu-running.gif"
+            alt="loading"
+          ></img>
+          <span>
+            <Typewriter
+              words={["Hang tight! Pikachu's almost there.."]}
+              cursor
+              loop={0}
+              typeSpeed={35}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </span>
+        </div>
+      ) : (
+        // The container for pokemon cards
+        <div className="flex justify-center items-center flex-wrap gap-15 mt-10">
+          {pokemons.map((pokemon) => {
+            const id = pokemon.url.split("/")[6]; // extracting id
+            const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`; // the url for image
+            return (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.8 }}
+                key={pokemon.name}
+              >
+                <Card name={`${pokemon.name}`} imgUrl={`${imgUrl}`} />
+              </motion.div>
+            );
+          })}
+        </div>
+      )}
     </>
   );
 };
