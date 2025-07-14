610db17ee7fadc802bd38fa1779d8ed854c492b5
Gamerk786po <abdullahpper@gmail.com>
2025-07-15 04:48:44 +0500

    Needed Data extracted from API data

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { PokemonProvider } from './context/PokemonContext/PokemonProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PokemonProvider>
     <App />
-    </PokemonProvider>
   </StrictMode>,
 )
