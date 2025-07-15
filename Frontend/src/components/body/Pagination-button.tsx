15062e4e88581f68f78fd86c9afe18f7dbbc4b09
Gamerk786po <abdullahpper@gmail.com>
2025-07-15 21:13:41 +0500

    Hover effect of buttons updated

--- Frontend/src/components/body/Pagination-button.tsx
+++ Frontend/src/components/body/Pagination-button.tsx
@@ -8,9 +8,8 @@ interface PaginationButtonProps{
 const PaginationButton: React.FC<PaginationButtonProps> = ({label, onClick, disabled}) => {
     return(
         <div>
-            <button className={`relative overflow-hidden group p-3 capitalize md:uppercase text-[15px] font-bold text-white ${disabled ? "opacity-50 cursor-not-allowed" : "cursor-pointer"} bg-red-500 rounded-xl `} onClick={onClick}>
-            <span className="relative z-10">{label}</span>
-            <span className="absolute left-0 top-0 w-full h-full bg-[#001f3f] transform translate-y-full group-hover:translate-y-5 transition-transform duration-300 ease-in-out z-0 rounded-t-[20%]"></span>
+            <button className={`p-3 capitalize md:uppercase text-[15px] font-bold text-white ${disabled ? "opacity-50 cursor-not-allowed" : "cursor-pointer"} bg-red-500 hover:bg-red-600 transition-all duration-200 ease-in-out rounded-xl `} onClick={onClick}>
+            <span className="">{label}</span>
             </button>
         </div>
     )
