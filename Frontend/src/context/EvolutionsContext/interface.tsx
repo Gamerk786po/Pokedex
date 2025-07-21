a1da80af59646a0c815249ee779be7b1ec67b14e
Gamerk786po <abdullahpper@gmail.com>
2025-07-21 23:42:17 +0500

    Evolutions interface created

--- Frontend/src/context/EvolutionsContext/interface.tsx
+++ Frontend/src/context/EvolutionsContext/interface.tsx
@@ -1,4 +1,18 @@
-// Interface for PokemonSpecies
+
+// Interface for EvolutionDetails
+interface EvolutionDetails{
+    min_level: number | null;
+    min_happiness: number | null;
+    held_item: string | null;
+    time_of_day: string;
+    item: string | null;
+    trigger: string;
+}
+
+
+// Interface for PokemonEvolutions
 export interface PokemonEvolutionsInterface {
-    // Still have to write interface
+    name: string;
+    evo_details: EvolutionDetails;
+    evo_to: PokemonEvolutionsInterface;
 }
