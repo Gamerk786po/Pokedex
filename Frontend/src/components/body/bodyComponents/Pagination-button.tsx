dec968675da595a94c8274de0c5b2381d0f105f5
Gamerk786po <abdullahpper@gmail.com>
2025-08-14 15:33:53 +0500

    Responsivness for 4k screens

--- Frontend/src/components/body/bodyComponents/Pagination-button.tsx
+++ Frontend/src/components/body/bodyComponents/Pagination-button.tsx
@@ -8,8 +8,12 @@ interface PaginationButtonProps{
 const PaginationButton: React.FC<PaginationButtonProps> = ({label, onClick, disabled}) => {
     return(
         <div>
-            <button className={`p-3 capitalize md:uppercase text-[15px] font-bold text-white ${disabled ? "opacity-50 cursor-not-allowed" : "cursor-pointer"} bg-red-500 hover:bg-red-600 transition-all duration-200 ease-in-out rounded-xl `} onClick={onClick}>
-            <span className="">{label}</span>
+            <button 
+                className={`p-3 2xl:p-6 capitalize md:uppercase text-[15px] 2xl:text-[22px] font-bold text-white ${disabled ? "opacity-50 cursor-not-allowed" : "cursor-pointer"} bg-red-500 hover:bg-red-600 transition-all duration-200 ease-in-out rounded-2xl`} 
+                onClick={onClick}
+                disabled={disabled}
+            >
+                <span className="">{label}</span>
             </button>
         </div>
     )
--- Frontend/src/components/body/bodyComponents/card.tsx
+++ Frontend/src/components/body/bodyComponents/card.tsx
@@ -1,18 +1,31 @@
 // Props for the Card
-interface CardProps{
-    name: string;
-    imgUrl: string;
+interface CardProps {
+  name: string;
+  imgUrl: string;
 }
 
 // The component for card
-const Card: React.FC<CardProps> = ({name, imgUrl}) => {
-    return(
-        // The container for the pokemon card
-        <div className="h-[148.5px] max-h-[180px] w-[122px] group flex flex-col gap-1 shadow-2xl items-center justify-center border border-blue-300 rounded-xl transition-all duration-300 hover:cursor-pointer hover:border-blue-500">
-            <img className="h-30 drop-shadow-[0_4px_6px_rgba(0,0,0,0.3)] transition-transform duration-300 group-hover:-translate-y-1" src={`${imgUrl}`} loading="lazy" alt={`${name}`}></img>
-            <p className="capitalize md:uppercase text-[15px] font-bold text-white transition-transform duration-300 group-hover:-translate-y-1">{name}</p>
-        </div>
-    )
-}
+const Card: React.FC<CardProps> = ({ name, imgUrl }) => {
+  return (
+    <div
+      className="h-[148.5px] w-[122px] group flex flex-col gap-1 shadow-2xl items-center justify-center border border-blue-300 rounded-xl transition-all duration-300 hover:cursor-pointer hover:border-blue-500 
+    2xl:h-[300px] 2xl:w-[240px]"
+    >
+      <img
+        className="drop-shadow-[0_4px_6px_rgba(0,0,0,0.3)] transition-transform duration-300 group-hover:-translate-y-1 
+        h-[120px] 2xl:h-[200px]"
+        src={imgUrl}
+        loading="lazy"
+        alt={name}
+      />
+      <p
+        className="capitalize md:uppercase text-[15px] font-bold text-white transition-transform duration-300 group-hover:-translate-y-1 
+        2xl:text-[28px]"
+      >
+        {name}
+      </p>
+    </div>
+  );
+};
 // Exporting
 export default Card;
