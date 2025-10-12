13e03cb12cc7446da42ebac2f7dd45a40542f8d9
Gamerk786po <abdullahpper@gmail.com>
2025-10-12 16:58:38 +0500

    HomeBody responsivness for all screens

--- Frontend/src/components/HomeBody/HomeBody.tsx
+++ Frontend/src/components/HomeBody/HomeBody.tsx
@@ -19,20 +19,29 @@ const HomeBody = () => {
   return (
     <>
       {/* Div containing Home content */}
-      <div className="mt-30 flex flex-col gap-4 justify-center items-center">
-        <h1 className="text-4xl font-bold"> Welcome to the Pokedex! </h1>
-        <p className="text-2xl font-light">
-          Explore detail information about Pokemons and manage your competitive
-          teams
+      <div className="mt-16 md:mt-24 xl:mt-30 pb-10 px-4 flex flex-col gap-6 justify-center items-center w-full text-center">
+        <h1 className="text-3xl sm:text-4xl xl:text-5xl font-bold">
+          Welcome to the Pokedex!
+        </h1>
+
+        <p className="text-lg sm:text-2xl xl:text-3xl font-light max-w-[80rem]">
+          Explore detailed information about Pokemons and manage your
+          competitive teams
         </p>
+
         {/* Div containing buttons */}
-        <div className="flex flex-row flex-wrap gap-3">
+        <div className="flex flex-row flex-wrap justify-center gap-3 w-full sm:w-auto">
           {navigationLinks.map((navLink) => (
-            <NavigationButton label={navLink.label} href={navLink.href} />
+            <NavigationButton
+              key={navLink.href}
+              label={navLink.label}
+              href={navLink.href}
+            />
           ))}
         </div>
       </div>
     </>
   );
 };
+
 export default HomeBody;
