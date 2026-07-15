91bfa800d0113aceb682ca42eecc4d04b2a030de
Gamerk786po <abdullahpper@gmail.com>
2026-07-15 21:38:50 +0500

    player.model.ts is completed

--- Backend/src/models/player.model.ts
+++ Backend/src/models/player.model.ts
@@ -1,5 +1,7 @@
 import { Schema, model } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { StringValue } from "ms";
 
 const playerSchema = new Schema(
   {
@@ -35,14 +37,65 @@ const playerSchema = new Schema(
 );
 
 // Hashing password before save
-playerSchema.pre("save", async function () {
+playerSchema.pre("save", async function (this: any) {
   if (!this.isModified("password")) return;
   this.password = await bcrypt.hash(this.password, 10);
 });
 
 // Method for comaparring password
-playerSchema.methods.isPasswordCorrect = async function (password: string) {
+playerSchema.methods.isPasswordCorrect = async function (
+  this: any,
+  password: string
+) {
   return await bcrypt.compare(password, this.password);
 };
 
+// Method for generating tokens
+playerSchema.methods.generateAccessToken = function (this: any) {
+  const secret = process.env.ACCESS_TOKEN_SECRET;
+  const expiry = process.env.ACCESS_TOKEN_EXPIRY as StringValue;
+
+  // Checking if secret is undefined
+  if (!secret) {
+    throw new Error(
+      "ACCESS_TOKEN_SECRET is not defined in the environment variables."
+    );
+  }
+
+  // Returning the token
+  return jwt.sign(
+    {
+      _id: this._id,
+      userName: this.userName,
+      email: this.email,
+    },
+    secret,
+    {
+      expiresIn: expiry,
+    }
+  );
+};
+
+// Generating refresh Token
+playerSchema.methods.generateRefreshToken = function (this: any) {
+  // Taking secret and expiry from .env
+  const secret = process.env.REFRESH_TOKEN_SECRET;
+  const expiry = process.env.REFRESH_TOKEN_EXPIRY as StringValue;
+  
+  // checking if secret is undefined or not
+  if (!secret)
+    throw new Error(
+      "REFRESH_TOKEN_SECRET is not defined in the environment variables."
+    );
+    // Generating refresh token
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    secret,
+    {
+      expiresIn: expiry,
+    }
+  );
+};
 export const Player = model("Player", playerSchema);
