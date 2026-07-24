b56e77d29e37f2f2903b61fed72d91c4b66c1429
Gamerk786po <abdullahpper@gmail.com>
2026-07-24 22:22:25 +0500

    solve DbConnect error

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,13 +1,14 @@
 import dotenv from "dotenv";
-import dbConnect from "./db/dbConnect";
-import app from "./app";
+import dbConnect from "./db/dbConnect.js";
+import app from "./app.js";
 dotenv.config(); // configuring dotenv
 
+const port = Number(process.env.PORT) || 4000
 // connecting with database
 dbConnect()
-  .then(() => {
-    app.listen(PORT, "0.0.0.0", () => {
-      console.log(`Server is runing that http://localhost:${PORT}.`);
+  .then(() => { 
+    app.listen(port, "0.0.0.0", () => {
+      console.log(`Server is runing that http://localhost:${port}.`);
     });
   })
   .catch((error) => {
